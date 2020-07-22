7061589657769d9b8e23fa4a6227beaaf27b5e3e
Thiago Margonar <thiago.henrique@sgsistemas.com.br>
2020-07-22 10:44:52 -0300

    cursoAula1 #in progress #time 1h 00m 0s #comment iniciado projeto em memoria

---
+++ resource/js/init.js
@@ -0,0 +1,7 @@
+<script src="../../angularjs/angular.js" ></script>
+<script>
+    angular.module("listaTelefonica",[]);
+    angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope){
+        $scope.app = "Lista Telefonica";
+    });
+</script>
